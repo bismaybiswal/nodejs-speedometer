90bfed0ecab614968a4a28d847144912bf84c94b
Bismay Biswal <45811843+bismaybiswal@users.noreply.github.com>
2020-02-19 13:15:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/speed.htm");
