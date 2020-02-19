9488be913dc63c518a41b9c9d3210ffe1d6b7f3f
Bismay Biswal <45811843+bismaybiswal@users.noreply.github.com>
2020-02-19 13:11:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.port || 3000
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/speed.htm");
 });
 
-var listener = app.listen(3000,function(){
+var listener = app.listen(PORT,function(){
     console.log("NodeJS server started (localhost:" + listener.address().port + ")");
 });
